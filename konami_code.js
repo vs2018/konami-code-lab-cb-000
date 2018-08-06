f4b125df0883fcca3c0cd4198ba57b4ece99a963
Vishal Sharma <vsh1819@gmail.com>
2018-08-06 05:18:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,25 +17,27 @@ let index = 0;
 function init() {
   // your code here
 
+  let index = 0
+
   document.body.addEventListener('keydown', function(event) {
   alert('I was clicked!')
 })
-  
+
 }
 
 
 // Keep track of index outside of the event handler.
- 
+
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = e.key;
- 
+
   if (key === alphabet[index]) {
     index++;
- 
+
     if (index === alphabet.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
