5461e3909d652e7251ce8a013a45e62a5b8f3053
Vishal Sharma <vsh1819@gmail.com>
2018-08-06 05:18:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -27,7 +27,7 @@ function init() {
     if (key === codes[index]) {
       index++;
 
-      if (index === alphabet.length) {
+      if (index === codes.length) {
         alert("Hurray!");
 
         index = 0;
@@ -40,22 +40,3 @@ function init() {
 
 }
 
-
-// Keep track of index outside of the event handler.
-
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = e.key;
-
-  if (key === alphabet[index]) {
-    index++;
-
-    if (index === alphabet.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
